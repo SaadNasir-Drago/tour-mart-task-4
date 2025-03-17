5d9a1ac0abe774c64ab29dc63dbf7cf56a766ef4
SaadNasir-Drago <snasir2194@gmail.com>
2025-03-17 15:26:56 +0600

    conflcit

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Injectable, NotFoundException } from "@nestjs/common"
 import { InjectRepository } from "@nestjs/typeorm"
 import type { Repository } from "typeorm"
@@ -49,55 +48,3 @@ export class NotificationsService {
   }
 }
 
-=======
-import { Injectable, NotFoundException } from "@nestjs/common"
-import { InjectRepository } from "@nestjs/typeorm"
-import type { Repository } from "typeorm"
-import { Notification } from "./entities/notification.entity"
-import { CreateNotificationDto } from "./dto/create-notification.dto"
-
-@Injectable()
-export class NotificationsService {
-  constructor(
-    @InjectRepository(Notification)
-    private notificationsRepository: Repository<Notification>,
-  ) {}
-
-  async findAll(userId: string, onlyUnread = false): Promise<Notification[]> {
-    const query = this.notificationsRepository
-      .createQueryBuilder("notification")
-      .where("notification.userId = :userId", { userId })
-      .orderBy("notification.createdAt", "DESC")
-
-    if (onlyUnread) {
-      query.andWhere("notification.isRead = :isRead", { isRead: false })
-    }
-
-    return query.getMany()
-  }
-
-  async create(createNotificationDto: CreateNotificationDto): Promise<Notification> {
-    const notification = this.notificationsRepository.create(createNotificationDto)
-    return this.notificationsRepository.save(notification)
-  }
-
-  async markAsRead(id: string, userId: string): Promise<Notification> {
-    const notification = await this.notificationsRepository.findOne({
-      where: { id, userId },
-    })
-
-    if (!notification) {
-      throw new NotFoundException(`Notification with ID ${id} not found`)
-    }
-
-    notification.isRead = true
-    return this.notificationsRepository.save(notification)
-  }
-
-  async createMany(notifications: CreateNotificationDto[]): Promise<Notification[]> {
-    const notificationEntities = this.notificationsRepository.create(notifications)
-    return this.notificationsRepository.save(notificationEntities)
-  }
-}
-
->>>>>>> master
